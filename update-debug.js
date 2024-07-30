03ba6484f065899df1f16bdf0533d49b1da0a7cb
fabriciocond <109551981+fabriciocond@users.noreply.github.com>
2024-07-30 04:47:28 -0300

    Update update-debug.js

--- update-debug.js
+++ update-debug.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const debugFilePath = path.join(__dirname, 'debug.txt');
+const debugFilePath = path.join(__dirname, 'domi/debug.txt');
 
 // Lógica para atualizar o arquivo debug.txt
 // Aqui você pode adicionar seu código para ler e modificar o arquivo debug.txt
