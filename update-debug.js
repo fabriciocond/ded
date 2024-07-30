db97ef161da50d813fadb28664b411e35ac7899f
fabriciocond <109551981+fabriciocond@users.noreply.github.com>
2024-07-30 05:46:08 -0300

    Update update-debug.js

--- update-debug.js
+++ update-debug.js
@@ -1,17 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
-// Certifique-se de que o caminho está correto
 const debugFilePath = path.join(__dirname, 'domi/debug.txt');
 
-// Lógica para atualizar o arquivo debug.txt
-const newData = "Novos dados para adicionar ao arquivo debug.txt\n";
+// Dados novos para substituir o conteúdo atual
+const newData = "Novos dados para substituir o conteúdo do arquivo debug.txt\n";
 
-fs.appendFile(debugFilePath, newData, (err) => {
+// Substitui o conteúdo atual do arquivo debug.txt
+fs.writeFile(debugFilePath, newData, (err) => {
   if (err) {
     console.error('Erro ao atualizar o arquivo debug.txt:', err);
-    process.exit(1); // Encerra com erro
-  } else {
-    console.log('Arquivo debug.txt atualizado com sucesso!');
+    process.exit(1);
   }
+  console.log('Arquivo debug.txt atualizado com sucesso!');
 });
